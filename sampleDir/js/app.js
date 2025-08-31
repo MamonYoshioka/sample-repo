119a88c78ace42ce2c055c53eb922d81e408ea00
Mamon Yoshioka <yoshiokamamon@gmail.com>
2025-08-31 14:20:17 +0900

    fix index.html app.js

--- sampleDir/js/app.js
+++ sampleDir/js/app.js
@@ -1,5 +1,19 @@
 const button = document.querySelector('#btn');
+const links = document.querySelectorAll('header a[data-href]');
 
-button.addEventListener('click', () => {
-    location.href = 'index.html';
+if(button){
+    button.addEventListener('click', () => {
+        location.href = 'index.html';
+    });
+}
+
+
+links.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        const link = item.getAttribute('data-href');
+        if(link){
+            location.href = link;
+        }
+    });
 });
